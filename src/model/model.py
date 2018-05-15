b511c8cf26d9140e4edb9b1635bee167467cc4b7
Sagar Saxena <saxenasa@tcd.ie>
2018-05-16 00:53:24 +0100

    File y_test generated along with all features

--- src/model/model.py
+++ src/model/model.py
@@ -1,11 +1,12 @@
 from sklearn.model_selection import KFold
 import sys
-sys.path.insert(0,'/home/sagar/Desktop/seldon/src')
+sys.path.append('./src')
 from preprocessing import Preprocessing
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+import os
 
 class Model(object):
     """docstModel."""
@@ -15,40 +16,41 @@ class Model(object):
     #making a split of 100 folds(datapoints) each in a split so that we have 10 slices
     def kfold_crossvalidation(self):
         number_of_folds = KFold(n_splits=10)
-        #np.append(self.new_feature_file,)
-        #number_of_folds.get_n_splits(self.new_feature_file)
         #splitting the new dataset created post PCA into training set and test set depending on the k-fold value
         for train_index, test_index in number_of_folds.split(self.new_feature_file):
-            #print("TRAIN:", train_index, "TEST:", test_index)
             new_feature_file_train, new_feature_file_test = self.new_feature_file[train_index], self.new_feature_file[test_index]
-            #print("TRAIN:", new_feature_file_train, "TEST:", new_feature_file_test)
-        #print("new_feature_file_train=",new_feature_file_train)
         return new_feature_file_train, new_feature_file_test
 
     #creating multivariate linear regression model
-    def linearRegModel(self, training_dataset, test_dataset):
+    def linearRegModel(self, training_dataset, test_dataset, dataset_selected_features):
         reg = LinearRegression()
-        X = training_dataset[:,0:3]
-        Y = training_dataset[:,2]
+        X = training_dataset[:,0:20]
+        Y = training_dataset[:,20]
         reg.fit(X, Y)
-        X_pred = test_dataset[:,0:3]
-        Y_pred = test_dataset[:,2]
+        X_pred = test_dataset[:,0:20]
+        Y_pred = test_dataset[:,20]
         y_pred = reg.predict(X_pred)
-        #print('RMSE Score: ', mean_squared_error(Y_pred, y_pred))
+        print('RMSE Score: ', mean_squared_error(Y_pred, y_pred))
         #predicting y_test based on the x_test dataset provided
-        X_test_array = np.array(pd.read_csv("/home/sagar/Desktop/seldon/datasets/X_test.csv"))
-        X_test = X_test_array[:,1:4]
+        X_test_array = np.array(pd.read_csv("./datasets/X_test.csv"))
+        X_test = X_test_array[:,1:21]
         y_test = reg.predict(X_test)
-        print("y_test: ",y_test)
+        #writing y_test to .csv
+        for i in range(y_test.size):
+            d = {'index':range(10000),'target':y_test}
+            df = pd.DataFrame(data = d,columns = ['index','target'])
+            df.set_index('index', inplace=True)
+        df.to_csv('./y_test.csv', encoding='utf-8')
+
+
 
 def main():
 
-    preprocessed_dataset = Preprocessing("/home/sagar/Desktop/seldon/datasets/X_train.csv")
-    normalised_dataset = preprocessed_dataset.read_normalise_dataset()
-    # #new_feature_dataset = preprocessed_dataset.pca_analysis_dataset(normalised_dataset)
+    preprocessed_dataset = Preprocessing(os.path.abspath("./datasets/X_train.csv"))
+    normalised_dataset,dataset_selected_features = preprocessed_dataset.read_normalise_dataset()
     model_creation = Model(normalised_dataset)
     train, test = model_creation.kfold_crossvalidation()
-    model_training = model_creation.linearRegModel(train, test)
+    model_training = model_creation.linearRegModel(train, test, dataset_selected_features)
 
 
 if __name__ == '__main__':
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-import matplotlib.pyplot as plt;plt.rcdefaults()
 import plotly.plotly as py
 import plotly.graph_objs as graphobj
+import sys
+sys.path.append('./src')
 import plotly
 plotly.tools.set_credentials_file(username='saxena-sagar', api_key='1HoemJdbXGB5YHKZ84GW')
+
 
 class Preprocessing(object):
     """
@@ -22,36 +24,30 @@ class Preprocessing(object):
         read_data_set = pd.read_csv(self.fname)
         dataset_array = np.array(read_data_set)
         #selecting the two features from the X_train dataset
-        dataset_selected_features = dataset_array[:,1:4]
-        #print(dataset_array.shape)
+        dataset_selected_features = dataset_array[:,1:21]
         #appending y_train to x_train's selected features post analysis via PCA and creating a dataset to train the model
-        y_train_read = pd.read_csv("/home/sagar/Desktop/seldon/datasets/y_train.csv")
+        y_train_read = pd.read_csv("./datasets/y_train.csv")
         y_train_dataset_array = np.array(y_train_read)
-        y_train_dataset = y_train_dataset_array[:,1:4]
+        y_train_dataset = y_train_dataset_array[:,1:2]
         training_dataset = np.append(dataset_selected_features,y_train_dataset,axis=1)
-        #print(training_dataset)
 
         #scaling the dataset array to remove outliers and keeing a low magnitude of the values
         scaled_dataset = preprocessing.scale(training_dataset)
         #Normalizing the dataset values
         dataset_normalized = preprocessing.normalize(scaled_dataset, norm='l2')
-        #replacing the missing value from the dataset treated by numpy as NaN.
-        # imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-        # imp.fit([[1, 2], [np.nan, 3], [7, 6]])
-        return dataset_normalized
-        #return training_dataset
+
+        return dataset_normalized,dataset_selected_features
+
 
     def pca_analysis_dataset(self,standardized_normalised_dataset):
         #calculating the mean vector where each value in this vector represents the sample mean of a feature column in the dataset
         dataset_mean_vec = np.mean(standardized_normalised_dataset, axis=0)
         #calculating the covariance matrix
         dataset_cov_matrix = (standardized_normalised_dataset - dataset_mean_vec).T.dot((standardized_normalised_dataset - dataset_mean_vec)) / (standardized_normalised_dataset.shape[0]-1)
-        #print('Covariance matrix \n%s' %cov_mat)
         #calculating the eighen decomposition on the covariance matrix
         dataset_eig_vals, dataset_eig_vecs = np.linalg.eig(dataset_cov_matrix)
         #determining the eighen vectors with lowest eighen values to be dropped for a lower-dimensional subspace, by ranking eighenvalues from highest to lowest in order
         dataset_eig_pairs = [(np.abs(dataset_eig_vals[i]), dataset_eig_vecs[:,i]) for i in range(len(dataset_eig_vals))]
-        #print("dataset_eig_pairs= \n",dataset_eig_pairs)
         #using explained variance to chose the major principle components
         total_variance_eig_vals = sum(dataset_eig_vals)
         expected_variance = [(i / total_variance_eig_vals)*100 for i in sorted(dataset_eig_vals, reverse = True)]
@@ -75,29 +71,15 @@ class Preprocessing(object):
                 title='Explained variance by different principal components')
         fig = graphobj.Figure(data=graph_data, layout=graph_layout)
         py.iplot(fig)
-        #Reducing the 20 dimension feature space to 2 dimension as per the PCA
+        #Reducing the 20 dimension feature space to 2 dimension as per the PCA to check the RMSE for two features
         new_feature_space_matrix = np.hstack((dataset_eig_pairs[0][1].reshape(21,1),dataset_eig_pairs[1][1].reshape(21,1)))
-        #print('new_feature_space_matrix:\n', new_feature_space_matrix)
 
         #projecting onto the new feature subspace
         new_feature_space = standardized_normalised_dataset.dot(new_feature_space_matrix)
-        #print("new feature space size = ",new_feature_space.shape)
-
-        # plt.plot(new_feature_space[0:5000,0], new_feature_space[0:5000,1], 'o', markersize=7, color='blue', alpha=0.5, label='class1')
-        # plt.plot(new_feature_space[5000:10000,0], new_feature_space[5000:10000,1], '^', markersize=7, color='red', alpha=0.5, label='class2')
-        #
-        # plt.xlim([-0.9,0.9])
-        # plt.ylim([-0.9,0.9])
-        # plt.xlabel('x_values')
-        # plt.ylabel('y_values')
-        # plt.legend()
-        # plt.title('New feature space post PCA with class labels')
-        #
-        # plt.show()
         return new_feature_space
 
 def main():
-    test = Preprocessing("/home/sagar/Desktop/seldon/datasets/X_train.csv")
+    test = Preprocessing("./datasets/X_train.csv")
     normalised_dataset = test.read_normalise_dataset()
     new_feature_dataset = test.pca_analysis_dataset(normalised_dataset)
 
